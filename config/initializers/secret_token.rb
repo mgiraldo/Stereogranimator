7f8a874cbd552dfa19a0fd4b80a67133ba534374
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2011-11-23 15:44:43 -0500

    Converted to Rails, added base framework. Added remote links to images.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stereo::Application.config.secret_token = '1ce08dc263a6ab2cd1390643e951da07a331b7339aa8a60725dd2d71c344edeb6799a5df401851f5a4f80cb448801173081fa468314550c6676d5403ae90faa7'
