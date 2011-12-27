15543184e293c3fdc5d12bf6f213fa4432f09cf8
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2011-12-27 18:44:47 -0500

    fixed type in gallery controller

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,7 +2,7 @@ class GalleryController < ApplicationController
   def index
     page = params[:page] == nil ? 1 : params[:page]
     @type = params[:type] == nil ? "all" : params[:type]
-    if type == "all"
+    if @type == "all"
       @images = Animation.order('created_at DESC').page(page).per(18)
     elsif @type == "gif" || @type == "anaglyph"
       @images = Animation.where("mode = ?", @type.upcase).order('created_at DESC').page(page).per(18)
