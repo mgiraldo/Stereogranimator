9d24fe60336a516089d88949bac1e506806a4052
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2012-01-04 17:04:35 -0500

    Sharing

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Stereo::Application.initialize!
+
+=begin
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.someserver.net",
+  :port  => 25,
+  :user_name  => "someone@someserver.net",
+  :password  => "mypass",
+  :authentication  => :login
+}
+=end
