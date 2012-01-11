cb3e8498988f2e29c5b5d9a535509a7a635e768a
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2012-01-11 18:41:39 -0500

    Image creation fix

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -46,63 +46,65 @@ class Animation < ActiveRecord::Base
     return @images
   end
   def createImage
-    # do some image magick
-    # first get each frame
-    im = Magick::Image.read(self.url).first
+    if self.filename==nil && self.digitalid!=nil
+      # do some image magick
+      # first get each frame
+      im = Magick::Image.read(self.url).first
+        
+      # TODO: proper crop of thumbnails (?)
+      fr1 = im.crop(self.x1,self.y1,self.width,self.height,true)
+      fr2 = im.crop(self.x2,self.y2,self.width,self.height,true)
       
-    # TODO: proper crop of thumbnails (?)
-    fr1 = im.crop(self.x1,self.y1,self.width,self.height,true)
-    fr2 = im.crop(self.x2,self.y2,self.width,self.height,true)
-    
-    # future filename
-    unique = Digest::SHA1.hexdigest('nyplsalt' + Time.now.to_s)
-    
-    if self.mode == "GIF"
-      # ANIMATED GIF!
-      self.filename = unique + ".gif"
+      # future filename
+      unique = Digest::SHA1.hexdigest('nyplsalt' + Time.now.to_s)
       
-      final = Magick::ImageList.new
-      final << fr1
-      final << fr2
-      final.ticks_per_second = 1000
-      final.delay = self.delay
-      final.iterations = 0
+      if self.mode == "GIF"
+        # ANIMATED GIF!
+        self.filename = unique + ".gif"
+        
+        final = Magick::ImageList.new
+        final << fr1
+        final << fr2
+        final.ticks_per_second = 1000
+        final.delay = self.delay
+        final.iterations = 0
+        
+        fr3 = fr1.resize_to_fill(140,140)
+        fr4 = fr2.resize_to_fill(140,140)
+        thumb = Magick::ImageList.new
+        thumb << fr3
+        thumb << fr4
+        thumb.ticks_per_second = 1000
+        thumb.delay = self.delay
+        thumb.iterations = 0
+      else
+        # ANAGLYPH!
+        self.filename = unique + ".jpg"
+        
+        redlayer = Magick::Image.new(self.width,self.height){self.background_color = "#f00"}
+        cyanlayer = Magick::Image.new(self.width,self.height){self.background_color = "#0ff"}
+        
+        fr1 = redlayer.composite(fr1, 0, 0, Magick::ScreenCompositeOp)
+        fr2 = cyanlayer.composite(fr2, 0, 0, Magick::ScreenCompositeOp)
+        final = fr1.composite(fr2, 0, 0, Magick::MultiplyCompositeOp)
+  
+        thumb = final.resize_to_fill(140,140)
+      end
       
-      fr3 = fr1.resize_to_fill(140,140)
-      fr4 = fr2.resize_to_fill(140,140)
-      thumb = Magick::ImageList.new
-      thumb << fr3
-      thumb << fr4
-      thumb.ticks_per_second = 1000
-      thumb.delay = self.delay
-      thumb.iterations = 0
-    else
-      # ANAGLYPH!
-      self.filename = unique + ".jpg"
+      # gotta packet the file
+      final.write("#{Rails.root}/tmp/#{self.filename}") { self.quality = 100 }
+      # and the thumb
+      thumbname = "t_" + self.filename
+      thumb.write("#{Rails.root}/tmp/#{thumbname}") { self.quality = 100 }
       
-      redlayer = Magick::Image.new(self.width,self.height){self.background_color = "#f00"}
-      cyanlayer = Magick::Image.new(self.width,self.height){self.background_color = "#0ff"}
-      
-      fr1 = redlayer.composite(fr1, 0, 0, Magick::ScreenCompositeOp)
-      fr2 = cyanlayer.composite(fr2, 0, 0, Magick::ScreenCompositeOp)
-      final = fr1.composite(fr2, 0, 0, Magick::MultiplyCompositeOp)
-
-      thumb = final.resize_to_fill(140,140)
+      # upload to Amazon S3
+      s3 = AWS::S3.new
+      bucket = s3.buckets['stereogranimator']
+      obj = bucket.objects[self.filename]
+      obj.write(:file => "#{Rails.root}/tmp/#{self.filename}", :acl => :public_read, :metadata => { 'photo_from' => 'New York Public Library' })
+      obj = bucket.objects[thumbname]
+      obj.write(:file => "#{Rails.root}/tmp/#{thumbname}", :acl => :public_read, :metadata => { 'photo_from' => 'New York Public Library' })
     end
-    
-    # gotta packet the file
-    final.write("#{Rails.root}/tmp/#{self.filename}") { self.quality = 100 }
-    # and the thumb
-    thumbname = "t_" + self.filename
-    thumb.write("#{Rails.root}/tmp/#{thumbname}") { self.quality = 100 }
-    
-    # upload to Amazon S3
-    s3 = AWS::S3.new
-    bucket = s3.buckets['stereogranimator']
-    obj = bucket.objects[self.filename]
-    obj.write(:file => "#{Rails.root}/tmp/#{self.filename}", :acl => :public_read, :metadata => { 'photo_from' => 'New York Public Library' })
-    obj = bucket.objects[thumbname]
-    obj.write(:file => "#{Rails.root}/tmp/#{thumbname}", :acl => :public_read, :metadata => { 'photo_from' => 'New York Public Library' })
   end
   def url
     "http://images.nypl.org/index.php?id=#{digitalid}&t=w"
