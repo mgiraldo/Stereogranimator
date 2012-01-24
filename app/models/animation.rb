82f773e7f1e618d26261cdce5054e084b8ee9cad
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2012-01-24 13:49:12 -0500

    Converted to command-line ImageMagick. Minor fixes.

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -57,12 +57,12 @@ class Animation < ActiveRecord::Base
     if (self.filename==nil && self.digitalid!=nil) || bypass
       # do some image magick
       # first get each frame
-      im = Magick::Image.read(self.url).first
-        
-      # TODO: proper crop of thumbnails (?)
-      fr1 = im.crop(self.x1,self.y1,self.width,self.height,true)
-      fr2 = im.crop(self.x2,self.y2,self.width,self.height,true)
+      im = "'#{self.url}'"
       
+      basepath = "#{Rails.root}/tmp/"
+      
+      fr1 = "#{im} -crop #{self.width}x#{self.height}+#{self.x1}+#{self.y1} +repage"#im.crop(self.x1,self.y1,self.width,self.height,true)
+      fr2 = "#{im} -crop #{self.width}x#{self.height}+#{self.x2}+#{self.y2} +repage"#im.crop(self.x2,self.y2,self.width,self.height,true)
       # future filename
       unique = Digest::SHA1.hexdigest('nyplsalt' + Time.now.to_s)
       
@@ -70,46 +70,81 @@ class Animation < ActiveRecord::Base
         # ANIMATED GIF!
         self.filename = self.filename==nil ? unique + ".gif" : self.filename
         
-        final = Magick::ImageList.new
-        final << fr1
-        final << fr2
-        final.ticks_per_second = 1000
-        final.delay = self.delay
-        final.iterations = 0
+#        final = Magick::ImageList.new
+#        final << fr1
+#        final << fr2
+#        final.ticks_per_second = 1000
+#        final.delay = self.delay
+#        final.iterations = 0
         
-        fr3 = fr1.resize_to_fill(140,140)
-        fr4 = fr2.resize_to_fill(140,140)
-        thumb = Magick::ImageList.new
-        thumb << fr3
-        thumb << fr4
-        thumb.ticks_per_second = 1000
-        thumb.delay = self.delay
-        thumb.iterations = 0
+#        fr3 = fr1.resize_to_fill(140,140)
+#        fr4 = fr2.resize_to_fill(140,140)
+#        thumb = Magick::ImageList.new
+#        thumb << fr3
+#        thumb << fr4
+#        thumb.ticks_per_second = 1000
+#        thumb.delay = self.delay
+#        thumb.iterations = 0
+        
+        tmp1 = "convert #{fr1} #{basepath}tmp1#{self.filename}"
+#        puts("TMP1: [#{tmp1}]")
+        system(tmp1)
+        tmp2 = "convert #{fr2} #{basepath}tmp2#{self.filename}"
+#        puts("TMP2: [#{tmp2}]")
+        system(tmp2)
+        final = "convert -delay #{self.delay}x1000 -loop 0 #{basepath}tmp*#{self.filename} #{basepath}#{self.filename}"
       else
         # ANAGLYPH!
         self.filename = self.filename==nil ? unique + ".png" : self.filename
         
-        fr1 = fr1.gamma_correct(1,0,0)
-        fr2 = fr2.gamma_correct(0,1,1)
-        final = fr2.composite fr1, Magick::CenterGravity, Magick::ScreenCompositeOp
-  
-        thumb = final.resize_to_fill(140,140)
+        fr1 = "#{fr1} -gamma 1,0,0" #fr1.gamma_correct(1,0,0)
+        # save temp fr1 for composite
+        tmp1 = "convert #{fr1} #{basepath}tmp1#{self.filename}"
+#        puts("TMP1: [#{tmp1}]")
+        system(tmp1)
+        fr2 = "#{fr2} -gamma 0,1,1" #fr2.gamma_correct(0,1,1)
+        # save temp fr2 for compsite
+        tmp2 = "convert #{fr2} #{basepath}tmp2#{self.filename}"
+#        puts("TMP1: [#{tmp2}]")
+        system(tmp2)
+        # the final composite anaglyph
+        final = "composite -compose screen #{basepath}tmp1#{self.filename} #{basepath}tmp2#{self.filename} #{basepath}#{self.filename}" #fr2.composite fr1, Magick::CenterGravity, Magick::ScreenCompositeOp
       end
       
       format = self.mode=="GIF"?"gif":"png"
       # gotta packet the file
-      final.write("#{format}:#{Rails.root}/tmp/#{self.filename}") { self.quality = 100 }
-      # and the thumb
+      system(final)#final.write("#{format}:#{Rails.root}/tmp/#{self.filename}") { self.quality = 100 }
+#      puts("FINAL: [#{final}]")
+      
+      # thumbnail
       thumbname = "t_" + self.filename
-      thumb.write("#{format}:#{Rails.root}/tmp/#{thumbname}") { self.quality = 100 }
+      if self.width > self.height
+        t = "h"
+      elsif self.height > self.width
+        t = "v"
+      else 
+        t = "s"
+      end
+      
+      #only need to crop if not s (square)
+      if  t == "h"
+        crop = " -crop #{self.height}x#{self.height}+#{((self.width-self.height)/2).to_i}+0! "
+      elsif t == "v"
+        crop = " -crop #{self.width}x#{self.width}+0+#{((self.height-self.width)/2).to_i}! "
+      end
+      crop = "#{crop}"#final.resize_to_fill(140,140)
+
+      thumbcmd = "convert #{basepath}#{self.filename} +repage #{crop} -resize 140x140 #{basepath}#{thumbname}"
+#      puts("THUMB: [#{thumbcmd}]")
+      system(thumbcmd)#thumb.write("#{format}:#{Rails.root}/tmp/#{thumbname}") { self.quality = 100 }
       
       # upload to Amazon S3
       s3 = AWS::S3.new
       bucket = s3.buckets['stereo.nypl.org']
       obj = bucket.objects[self.filename]
-      obj.write(:file => "#{Rails.root}/tmp/#{self.filename}", :acl => :public_read, :metadata => { 'description' => self.metadata, 'photo_from' => 'New York Public Library' })
+      obj.write(:file => "#{basepath}#{self.filename}", :acl => :public_read, :metadata => { 'description' => self.metadata, 'photo_from' => 'New York Public Library' })
       obj = bucket.objects[thumbname]
-      obj.write(:file => "#{Rails.root}/tmp/#{thumbname}", :acl => :public_read, :metadata => { 'description' => self.metadata, 'photo_from' => 'New York Public Library' })
+      obj.write(:file => "#{basepath}#{thumbname}", :acl => :public_read, :metadata => { 'description' => self.metadata, 'photo_from' => 'New York Public Library' })
     end
   end
   def self.purgeBlacklisted
