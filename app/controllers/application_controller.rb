4d19bfda427718738230dfe152be07be2040463f
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2012-01-25 15:25:26 -0500

    Minor content changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def ensure_domain
     if request.env['HTTP_HOST'] != APP_DOMAIN
       # HTTP 301 is a "permanent" redirect
-      redirect_to "http://#{APP_DOMAIN}", :status => 301
+      redirect_to "http://#{APP_DOMAIN}", :status => 301 unless request.env['HTTP_HOST'] == 'localhost:3000'
     end
   end
   
