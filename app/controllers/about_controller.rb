891d253bb2c5353ddc0ec2d4bcae63392d6b47d4
Mauricio Giraldo <mauricio.giraldo@gmail.com>
2012-01-26 14:23:44 -0500

    Cache

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -5,32 +5,32 @@ class AboutController < ApplicationController
   
   def index
     ## LATEST IMAGES cache
-    if Rails.cache.exist("home_images") == nil do
+    if Rails.cache.fetch("home_images") == nil
       @images = Animation.where("creator != ?", 'siege').order('created_at DESC').limit(6)
       Rails.cache.write("home_images", @images, :expires_in => 5.minutes)
     else
       @images = Rails.cache.read("home_images")
     end
     ## ANAGLYPH COUNT cache
-    if Rails.cache.exist("home_anaglyphcount") == nil do
+    if Rails.cache.fetch("home_anaglyphcount") == nil
       @anaglyphcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','ANAGLYPH','siege').map(&:total)[0].to_i
       Rails.cache.write("home_anaglyphcount", @anaglyphcount, :expires_in => 5.minutes)
     else
-      @anaglyphcount = Rails.cache.read("home_anaglyphcount")
+      @anaglyphcount = Rails.cache.fetch("home_anaglyphcount")
     end
     ## GIF COUNT cache
-    if Rails.cache.exist("home_gifcount") == nil do
+    if Rails.cache.fetch("home_gifcount") == nil
       @gifcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','GIF','siege').map(&:total)[0].to_i
       Rails.cache.write("home_gifcount", @gifcount, :expires_in => 5.minutes)
     else
-      @gifcount = Rails.cache.read("home_gifcount")
+      @gifcount = Rails.cache.fetch("home_gifcount")
     end
     ## IMAGE COUNT cache
-    if Rails.cache.exist("home_imagecount") == nil do
+    if Rails.cache.fetch("home_imagecount") == nil
       @imagecount = Image.select('COUNT(id) as total').map(&:total)[0].to_i
       Rails.cache.write("home_imagecount", @imagecount, :expires_in => 5.minutes)
     else
-      @imagecount = Rails.cache.read("home_imagecount")
+      @imagecount = Rails.cache.fetch("home_imagecount")
     end
   end
 
