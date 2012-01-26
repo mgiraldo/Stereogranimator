f758df0c7fc111d042f8c7e8d261833eb09e0a23
Mauricio Giraldo <mauricio.giraldo@gmail.com>
2012-01-26 14:06:00 -0500

    No cache

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,12 +4,12 @@ class AboutController < ApplicationController
   end
   
   def index
-    Rails.cache.fetch("home", :expires_in => 5.minutes) do
+    #Rails.cache.fetch("home", :expires_in => 5.minutes) do
       @images = Animation.where("creator != ?", 'siege').order('created_at DESC').limit(6)
       @anaglyphcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','ANAGLYPH','siege').map(&:total)[0].to_i
       @gifcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','GIF','siege').map(&:total)[0].to_i
       @imagecount = Image.select('COUNT(id) as total').map(&:total)[0].to_i
-    end
+    #end
   end
 
   def animatedgif
