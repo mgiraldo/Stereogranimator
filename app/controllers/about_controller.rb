e9c9e56f4a6c94f4b3230398460ce8e7807d9cd6
Mauricio Giraldo <mauricio.giraldo@gmail.com>
2012-01-26 13:51:13 -0500

    Caching home

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,10 +3,12 @@ class AboutController < ApplicationController
   end
   
   def index
-    @images = Animation.where("creator != ?", 'siege').order('created_at DESC').limit(6)
-    @anaglyphcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','ANAGLYPH','siege').map(&:total)[0].to_i
-    @gifcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','GIF','siege').map(&:total)[0].to_i
-    @imagecount = Image.select('COUNT(id) as total').map(&:total)[0].to_i
+    Rails.cache.fetch("/") do
+      @images = Animation.where("creator != ?", 'siege').order('created_at DESC').limit(6)
+      @anaglyphcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','ANAGLYPH','siege').map(&:total)[0].to_i
+      @gifcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','GIF','siege').map(&:total)[0].to_i
+      @imagecount = Image.select('COUNT(id) as total').map(&:total)[0].to_i
+    end
   end
 
   def animatedgif
