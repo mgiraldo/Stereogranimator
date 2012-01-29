a846064d444b330d81b350046db92a9c20ac10e1
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2012-01-29 16:27:44 -0500

    Added random at home

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -8,6 +8,7 @@ class AboutController < ApplicationController
   def index
     @images = Animation.where("creator != ?", 'siege').order('created_at DESC').limit(6)
     @popular = Animation.where("creator != ?", 'siege').order('views DESC').limit(6)
+    @random = Animation.where("creator != ?", 'siege').order('random()').limit(6)
     @anaglyphcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','ANAGLYPH','siege').map(&:total)[0].to_i
     @gifcount = Animation.select('COUNT(id) as total').where('mode = ? AND creator != ?','GIF','siege').map(&:total)[0].to_i
     @imagecount = Image.select('COUNT(id) as total').map(&:total)[0].to_i
