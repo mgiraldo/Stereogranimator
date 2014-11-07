9fd0b16e98e8ee1c6c1ad192efcc3f0ef0364130
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2014-11-07 18:37:58 -0500

    :email: added email sending test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -87,7 +87,7 @@ class ApplicationController < ActionController::Base
   def ensure_domain
     if request.env['HTTP_HOST'] != APP_DOMAIN
       # HTTP 301 is a permanent redirect
-      redirect_to "http://#{APP_DOMAIN}#{request.fullpath}", :status => 302 unless request.env['HTTP_HOST'] == 'stereostaging.heroku.com' || request.env['HTTP_HOST'] == 'localhost:3000' || request.env['HTTP_HOST'].include?('xip.io:3000')
+      redirect_to "http://#{APP_DOMAIN}#{request.fullpath}", :status => 302 unless request.env['HTTP_HOST'] == 'stereostaging.herokuapp.com' || request.env['HTTP_HOST'] == 'stereostaging.heroku.com' || request.env['HTTP_HOST'] == 'localhost:3000' || request.env['HTTP_HOST'].include?('xip.io:3000')
     end
   end
   
