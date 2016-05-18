40cc4f7aff256c75fe8eb751b850598f6dd299e3
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-05-18 16:31:17 -0400

    :space_invader: removed https from flickr image get

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,6 +2,8 @@ class ImagesController < ApplicationController
   # GET /getimagedata/digitalid
   def getimagedata
     url = params[:url]
+    # remove the https (flickr doesnt like heroku in https)
+    url = url.gsub(/https:/, 'http:')
     puts url
     im = Magick::Image.read(url).first
     im.background_color = "none"
@@ -13,7 +15,7 @@ class ImagesController < ApplicationController
     if (im.rows > 600)
       im = im.resize_to_fit(800,600)
     end
-    
+
     im = im.rotate(params[:r].to_f)
     str = Base64.encode64(im.to_blob{self.format = "JPEG"})
     output = {
@@ -27,10 +29,12 @@ class ImagesController < ApplicationController
       format.jpeg { render :text => im.to_blob{self.format = "JPEG"}, :status => 200, :type => 'image/jpg' }
     end
   end
-  
+
   # GET /getpixels/digitalid
   def getpixels
     url = params[:url]
+    # remove the https (flickr doesnt like heroku in https)
+    url = url.gsub(/https:/, 'http:')
     im = Magick::Image.read(url).first
     # test for width (for HQ images)
     if (im.columns > 800)
@@ -44,17 +48,17 @@ class ImagesController < ApplicationController
     respond_to do |format|
       format.jpeg { render :text => im.to_blob{self.format = "JPEG"}, :status => 200, :type => 'image/jpg' }
     end
-    
+
   end
-  
+
   def verifyPhoto
     respond_to do |format|
       format.json { render :json => Image.verifyFlickrPhoto(params[:id]).to_json }
     end
   end
-  
+
   def test
     render :json => flickr.photosets.getInfo(:photoset_id => "72157604192771132")
   end
-  
+
 end
